dc5fb2f111c2076467439e57868ff371f801f9a7
butchigho <butchigho93@gmail.com>
2015-01-22 16:49:31 +0700

    sua new

---
+++ Database/newsphp.sql
@@ -0,0 +1,86 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : quyhoa
+Source Server Version : 50539
+Source Host           : localhost:3306
+Source Database       : newsphp
+
+Target Server Type    : MYSQL
+Target Server Version : 50539
+File Encoding         : 65001
+
+Date: 2015-01-22 16:48:47
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `categories`
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of categories
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `news`
+-- ----------------------------
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `category_id` int(6) NOT NULL,
+  `title` varchar(450) CHARACTER SET utf8 NOT NULL,
+  `desc` text CHARACTER SET utf8 NOT NULL,
+  `cont` text NOT NULL,
+  `image` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `date_create` datetime NOT NULL,
+  `date_update` datetime NOT NULL,
+  `user_id` int(6) NOT NULL,
+  `status` binary(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of news
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of roles
+-- ----------------------------
+INSERT INTO `roles` VALUES ('1', 'Admin');
+INSERT INTO `roles` VALUES ('2', 'Manager');
+INSERT INTO `roles` VALUES ('3', 'Editor');
+
+-- ----------------------------
+-- Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `role_id` int(6) NOT NULL,
+  `account` varchar(250) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fullname` varchar(250) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
